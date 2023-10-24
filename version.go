3740ee362fda72716e31cf5c42bd02167d2041ad
David Kröll <david.kroell@outlook.com>
2023-10-24 08:36:12 +0200

    add vcs info to version output

--- version.go
+++ version.go
@@ -1,6 +1,9 @@
 package edurouter
 
-import "fmt"
+import (
+	"fmt"
+	"runtime/debug"
+)
 
 const (
 	majorVersion = 0
@@ -9,5 +12,22 @@ const (
 )
 
 func Version() string {
-	return fmt.Sprintf("v%d.%d.%d", majorVersion, minorVersion, patchVersion)
+	return fmt.Sprintf("v%d.%d.%d %s", majorVersion, minorVersion, patchVersion, vcsInfo())
+}
+
+func vcsInfo() string {
+	var revision string
+	var time string
+
+	if info, ok := debug.ReadBuildInfo(); ok {
+		for _, setting := range info.Settings {
+			if setting.Key == "vcs.revision" {
+				revision = setting.Value
+			}
+			if setting.Key == "vcs.time" {
+				time = setting.Value
+			}
+		}
+	}
+	return "git:" + revision + ", at " + time
 }
